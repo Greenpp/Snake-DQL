75d151a2da5e307afeb04ba86f42f60c0b12fa82
Jakub Ciszek <jakubciszek64@gmail.com>
2020-07-07 19:05:18 +0200

    refactor

--- net.py
+++ net.py
@@ -1,136 +1,202 @@
-import os
 import random
 
-import numpy as np
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, IterableDataset
 from pytorch_lightning.core.decorators import auto_move_data
+from torch.utils.data import DataLoader, IterableDataset
 
-import wandb
 from game import Engine
 
 
-class Memory(IterableDataset):
-    def __init__(self, size, sample_size, iterations):
-        super(Memory).__init__()
+class Memory:
+    """
+    Memory holding given number of last experiences with ability to sample it random
+    """
+
+    def __init__(self, size):
         self.max_size = size
-        self.sample_size = sample_size
         self.buffer = []
-        self.iterations = iterations
 
-    def __iter__(self):
+    def sample(self, num):
+        """
+        Sample <num> random experiences
+        """
         buff_size = len(self.buffer)
-        sample_size = min(self.sample_size, buff_size)
+        sample_size = min(num, buff_size)
         sample = random.sample(self.buffer, sample_size)
-        for s in sample:
-            yield s
 
-    def append(self, experience):
-        self.buffer.append(experience)
+        return sample
+
+    def append(self, exp):
+        """
+        Add new experience and remove the oldest if capacity is reached
+        """
+        self.buffer.append(exp)
         if len(self.buffer) > self.max_size:
             self.buffer.pop(0)
 
 
+class RLDataset(IterableDataset):
+    """
+    Dataset serving stored in memory experiences
+    """
+
+    def __init__(self, memory, sample_size):
+        super(RLDataset).__init__()
+        self.memory = memory
+        self.sample_size = sample_size
+
+    def __iter__(self):
+        data = self.memory.sample(self.sample_size)
+
+        for d in data:
+            yield d
+
+
 class Agent:
-    def __init__(self, batch_size, memory_size, init_epsilon, end_epsilon, iterations_num, actions_num):
-        self.game_engine = Engine(board_size=20)
-        self.replay_memory = Memory(memory_size, batch_size, iterations_num)
+    """
+    Reinforcement learning agent interacting with the game environment
+    """
 
-        self.epsilon_delta = (init_epsilon - end_epsilon) / iterations_num
-        self.batch_size = batch_size
-        self.epsilon = init_epsilon
-        self.actions_num = actions_num
-        state = self.game_engine.get_board_state()
-        self.game_state = torch.from_numpy(state)
+    def __init__(self, model, memory_size, epoch_samples_num, game_board_size=20):
+        self.model = model
+        self.game_engine = Engine(game_board_size)
+        self.replay_memory = Memory(memory_size)
+        self.epoch_samples = epoch_samples_num
 
-    def move(self, model_output):
-        random_action = model_output is None or random.random() <= self.epsilon
-        action_idx = random.randint(0, self.actions_num - 1) if random_action else torch.argmax(model_output).item()
-        action = torch.zeros(self.actions_num)
+        self.game_state = self.game_engine.get_game_state()
+        self.game_state = torch.from_numpy(self.game_state).unsqueeze(0)
+
+    def get_dataset(self):
+        """
+        Create dataset using replay memory
+        """
+        ds = RLDataset(self.replay_memory, self.epoch_samples)
+
+        return ds
+
+    def move(self, epsilon):
+        """
+        Make single interaction with the game environment
+        """
+        actions_num = self.model.actions_num
+
+        rnd_action = random.random() <= epsilon
+        if rnd_action:
+            action_idx = random.randint(0, actions_num - 1)
+        else:
+            model_output = self.model(game_state)
+            action_idx = torch.argmax(model_output).item()
+        action = torch.zeros(actions_num)
         action[action_idx] = 1
+        action = action.unsqueeze(0)
 
-        new_state, reward, terminal = self.game_engine.next_round_nn(action_idx)
-        new_state = torch.from_numpy(new_state)
+        direction = self.translate_action(action_idx)
+        reward, terminal = self.game_engine.next_round(direction)
+        new_state = self.game_engine.get_game_state()
+        new_state = torch.from_numpy(new_state).unsqueeze(0)
 
-        self.replay_memory.append((self.game_state, action, reward, new_state, terminal))
-
-        self.game_state = new_state
-        self.epsilon -= self.epsilon_delta
+        exp = (self.game_state, action, reward, new_state, terminal)
+        self.replay_memory.append(exp)
 
         if terminal:
             self.game_engine.reset()
 
-    def get_board_state(self):
-        return self.game_state
+    def translate_action(self, action):
+        """
+        Translate action to new direction
+
+        Actions:
+        0 - turn left
+        1 - go straight
+        2 - turn right
+
+        """
+        direction = self.game_engine.direction
+        new_direction = (direction + action - 1) % 4
+
+        return new_direction
+
+    def warmup(self, num):
+        """
+        Make <num> of random moves
+        """
+        for _ in range(num):
+            self.move(1)
+
 
 class SnakeNet(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
         self.actions_num = 3
-        self.gamma = 0.9
-        self.final_epsilon = .0001
-        self.init_epsilon = .5
+        self.gamma = .9
+        self.epsilon_init = 1
+        self.epsilon_final = .0001
         self.iterations_num = 1000
         self.replay_memory_size = 10000
         self.batch_size = 32
 
-        self.agent = Agent(self.batch_size, self.replay_memory_size, self.init_epsilon,
-                           self.final_epsilon, self.iterations_num, self.actions_num)
-        self.it_num = 0
+        self.epsilon = self.epsilon_init
+        self.epsilon_delta = (self.epsilon_final -
+                              self.epsilon_init) / self.iterations_num
 
-        self.fc1 = nn.Linear(11, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, self.actions_num)
+        self.agent = Agent(self, self.replay_memory_size, 1000)
+        self.net = nn.Sequential(
+            nn.Linear(11, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.actions_num),
+            nn.Softmax(dim=1)
+        )
 
     @auto_move_data
     def forward(self, x):
         x = x.float()
 
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.softmax(self.fc4(x), dim=1)
-
-        return x
+        return self.net(x)
 
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=1e-5)
+        opt = optim.Adam(self.parameters(), lr=1e-5)
+        return opt
 
     def prepare_data(self):
-        self.agent.move(None)
+        return self.agent.warmup(1000)
+
+    def update_epsilon(self):
+        self.epsilon -= self.epsilon_delta
 
     def training_step(self, batch, batch_idx):
-        game_state = self.agent.get_board_state()
-        output = self(game_state.unsqueeze(0))
-
-        self.agent.move(output)
+        self.agent.move(self.epsilon)
+        self.update_epsilon()
 
         y, y_hat = batch
-
         loss = F.mse_loss(y, y_hat)
 
         return {'loss': loss}
 
     def train_dataloader(self):
-        return DataLoader(self.agent.replay_memory, batch_size=self.batch_size, collate_fn=self.custom_collate)
+        ds = self.agent.get_dataset()
+        dl = DataLoader(ds, batch_size=self.batch_size,
+                        collate_fn=self.custom_collate)
+
+        return dl
 
     def custom_collate(self, batch):
-        state_batch = torch.cat(tuple(d[0].unsqueeze(0) for d in batch))
-        action_batch = torch.cat(tuple(d[1].unsqueeze(0) for d in batch))
+        state_batch = torch.cat(tuple(d[0] for d in batch))
+        action_batch = torch.cat(tuple(d[1] for d in batch))
         reward_batch = tuple(d[2] for d in batch)
-        next_state_batch = torch.cat(tuple(d[3].unsqueeze(0) for d in batch))
+        new_state_batch = torch.cat(tuple(d[3] for d in batch))
         terminal_batch = tuple(d[4] for d in batch)
 
-        next_state_output = self(next_state_batch)
-
-        y_hat = torch.cat(tuple(torch.tensor([reward if terminal else reward + self.gamma * torch.max(next_output)]) for reward, terminal, next_output in zip(reward_batch, terminal_batch, next_state_output)))
-        y_hat.detach_()
+        new_state_output = self(new_state_batch)
+        y_hat = torch.tensor(tuple([reward if terminal else reward + self.gamma * torch.max(new_output)]
+                                   for reward, terminal, new_output in zip(reward_batch, terminal_batch, new_state_output)))
+        y_hat = y_hat.squeeze()
 
         pred = self(state_batch)
         action_batch = action_batch.type_as(pred)
@@ -142,10 +208,8 @@ class SnakeNet(pl.LightningModule):
 
 
 if __name__ == '__main__':
+    torch.autograd.set_detect_anomaly(True)
     model = SnakeNet()
-    trainer = pl.Trainer(
-        gpus=1
-    )
+    trainer = pl.Trainer(gpus=1)
     trainer.fit(model)
-
-    torch.save(model.state_dict(), './model.pt')
+    trainer.save_checkpoint('model.ptl')
